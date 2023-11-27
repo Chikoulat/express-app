2e489ff1bcc1122a8e0222f6631e1e58db08e33a
unknown <hamidouboum@hotmail.fr>
2023-11-27 12:03:31 +0100

    rexpress 01.1-TDD

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -8,18 +8,14 @@ describe("GET /api/movies", () => {
     expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.status).toEqual(200);
   });
-});
 
-describe("GET /api/movies/:id", () => {
   it("should return movie by id", async () => {
     const response = await request(app).get("/api/movies/1");
 
     expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.status).toEqual(200);
   });
-});
 
-describe("GET /api/movies/:id", () => {
   it("should return movie by id", async () => {
     const response = await request(app).get("/api/movies/0");
 
