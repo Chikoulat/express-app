2e4718c900fa89fbe8fae4de15f2347009b200de
chikoulat <hamidouboum@hotmail.fr>
2023-11-27 12:31:41 +0100

    modified:   tests/movies.test.js

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -4,21 +4,18 @@ const app = require("../app");
 describe("GET /api/movies", () => {
   it("should return all movies", async () => {
     const response = await request(app).get("/api/movies");
-
     expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.status).toEqual(200);
   });
 
   it("should return movie by id", async () => {
     const response = await request(app).get("/api/movies/1");
-
     expect(response.headers["content-type"]).toMatch(/json/);
     expect(response.status).toEqual(200);
   });
 
   it("should return 404", async () => {
     const response = await request(app).get("/api/movies/0");
-
     expect(response.status).toEqual(404);
   });
 });
